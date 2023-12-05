2ace6f50c92a48f63312cdfc64a2903fe643c633
smringel <seth.ringel@covermymeds.com>
2023-12-04 20:01:16 -0500

    d2c2

--- lib/d2c2.ex
+++ lib/d2c2.ex
@@ -1,4 +1,4 @@
-defmodule D2C1 do
+defmodule D2C2 do
   alias Utils.Parser
 
   @zeros %{"green" => 0, "blue" => 0, "red" => 0}
@@ -7,20 +7,14 @@ defmodule D2C1 do
     Parser.parse("d2/#{ext}")
     |> parse_games()
     |> Enum.map(&select_highest_colors(&1))
-    |> filter_possible_games()
-    |> sum_ids()
+    |> reduce_min_cubes()
   end
 
   def parse_games(games) do
     Enum.map(games, fn game ->
-      [id_string, results_string] = String.split(game, ":")
-
-      {id, _} = id_string
-        |> String.split(" ")
-        |> List.last()
-        |> Integer.parse()
-
-      rounds = results_string
+      game
+      |> String.split(":")
+      |> List.last()
       |> String.split(";")
       |> Enum.reduce([], fn round, acc ->
         round_results = round
@@ -33,31 +27,22 @@ defmodule D2C1 do
           end)
         acc ++ round_results
       end)
-      {id, rounds}
     end)
   end
 
-  def select_highest_colors({id, data}) do
-    highest_colors = data
-    |> Enum.reduce(@zeros, fn {num, color}, acc ->
+  def select_highest_colors(data) do
+    Enum.reduce(data, @zeros, fn {num, color}, acc ->
       if Map.get(acc, color, 0) < num do
         Map.put(acc, color, num)
       else
         acc
       end
     end)
-    {id, highest_colors}
   end
 
-  def filter_possible_games(games) do
-    Enum.filter(games, fn {_id, scores} ->
-      scores["blue"] <= 14 and
-      scores["green"] <= 13 and
-      scores["red"] <= 12
+  def reduce_min_cubes(games) do
+    Enum.reduce(games, 0, fn game, acc ->
+      Enum.reduce(Map.values(game), 1, &Kernel.*/2) + acc
     end)
-  end
-
-  def sum_ids(games) do
-    Enum.reduce(games, 0, fn {id, _game}, acc -> acc + id end)
   end
 end
