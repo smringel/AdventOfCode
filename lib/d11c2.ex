033a8b12ee0205f06cd5b5128e39286d0aaead63
smringel <seth.ringel@covermymeds.com>
2023-12-11 16:15:50 -0500

    d11c1

---
+++ lib/d11c2.ex
@@ -0,0 +1,59 @@
+defmodule D11C2 do
+  alias Utils.Parser
+
+  def run(ext) do
+    data = Parser.parse("d11/#{ext}")
+
+    map = data
+    |> expand_rows()
+    |> Enum.map(&String.graphemes/1)
+    |> expand_cols()
+
+    galaxies = find_galaxies(map)
+    Enum.reduce(galaxies, 0, fn galaxy, acc ->
+      Enum.map(galaxies, fn {x, y} -> abs(x - elem(galaxy, 0)) + abs(y - elem(galaxy, 1)) end)
+      |> Enum.sum()
+      |> Kernel.+(acc)
+    end)
+    |> Kernel./(2)
+    |> round()
+  end
+
+  def expand_rows(data) do
+    Enum.reduce(data, [], fn row, acc ->
+      if String.contains?(row, "#") do
+        acc ++ [row]
+      else
+        acc ++ [row, row]
+      end
+    end)
+  end
+
+  def expand_cols(data) do
+    empty_cols = Enum.reduce(0..length(data) - 1, [], fn x, acc ->
+      if Enum.all?(data, &Enum.at(&1, x) == ".") do
+        acc ++ [x]
+      else
+        acc
+      end
+    end)
+    |> Enum.reverse()
+
+    Enum.reduce(data, [], fn row, acc ->
+      acc ++ [Enum.reduce(empty_cols, row, fn col, row_acc ->
+        List.insert_at(row_acc, col, ".")
+      end)]
+    end)
+  end
+
+  def find_galaxies(map) do
+    Enum.reduce(0..length(map) - 1, [], fn y, acc ->
+      map
+      |> Enum.at(y)
+      |> Enum.with_index()
+      |> Enum.filter(fn {val, _x} -> val == "#" end)
+      |> Enum.map(fn {_val, x} -> {x, y} end)
+      |> then(& acc ++ &1)
+    end)
+  end
+end
