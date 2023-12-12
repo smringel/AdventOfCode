3468fa31273f8eb3b2bdab3602a32cafbf17f0b1
smringel <seth.ringel@covermymeds.com>
2023-12-11 23:12:16 -0500

    d11c2 almost

--- lib/d11c2.ex
+++ lib/d11c2.ex
@@ -1,48 +1,40 @@
 defmodule D11C2 do
   alias Utils.Parser
 
+  @mod 999_999
+
   def run(ext) do
     data = Parser.parse("d11/#{ext}")
-
-    map = data
-    |> expand_rows()
     |> Enum.map(&String.graphemes/1)
-    |> expand_cols()
 
-    galaxies = find_galaxies(map)
-    Enum.reduce(galaxies, 0, fn galaxy, acc ->
-      Enum.map(galaxies, fn {x, y} -> abs(x - elem(galaxy, 0)) + abs(y - elem(galaxy, 1)) end)
-      |> Enum.sum()
-      |> Kernel.+(acc)
-    end)
-    |> Kernel./(2)
-    |> round()
+    rows = find_empty_rows(data)
+    cols = find_empty_cols(data)
+
+    data
+    |> find_galaxies()
+    |> calc_distances(rows, cols, 0)
   end
 
-  def expand_rows(data) do
-    Enum.reduce(data, [], fn row, acc ->
-      if String.contains?(row, "#") do
-        acc ++ [row]
-      else
-        acc ++ [row, row]
-      end
-    end)
-  end
-
-  def expand_cols(data) do
-    empty_cols = Enum.reduce(0..length(data) - 1, [], fn x, acc ->
-      if Enum.all?(data, &Enum.at(&1, x) == ".") do
-        acc ++ [x]
-      else
+  def find_empty_rows(data) do
+    Enum.reduce(0..length(data) - 1, [], fn y, acc ->
+      data
+      |> Enum.at(y)
+      |> Enum.any?(& &1 == "#")
+      |> if do
         acc
+      else
+        acc ++ [y]
       end
     end)
-    |> Enum.reverse()
+  end
 
-    Enum.reduce(data, [], fn row, acc ->
-      acc ++ [Enum.reduce(empty_cols, row, fn col, row_acc ->
-        List.insert_at(row_acc, col, ".")
-      end)]
+  def find_empty_cols(data) do
+    Enum.reduce(0..length(data) - 1, [], fn x, acc ->
+      if Enum.any?(data, &Enum.at(&1, x) == "#") do
+        acc
+      else
+        acc ++ [x]
+      end
     end)
   end
 
@@ -54,6 +46,29 @@ defmodule D11C2 do
       |> Enum.filter(fn {val, _x} -> val == "#" end)
       |> Enum.map(fn {_val, x} -> {x, y} end)
       |> then(& acc ++ &1)
+    end)
+  end
+
+  def calc_distances([], _empty_rows, _empty_cols, sum), do: sum
+  def calc_distances([galaxy | galaxies], empty_rows, empty_cols, sum) do
+    Enum.map(galaxies, fn {x, y} ->
+      gal_x = elem(galaxy, 0)
+      gal_y = elem(galaxy, 1)
+      x_mod = empty_mod(x, gal_x, empty_cols) * @mod
+      y_mod = empty_mod(y, gal_y, empty_rows) * @mod
+      abs(x - gal_x) + abs(y - gal_y) + x_mod + y_mod
+    end)
+    |> Enum.sum()
+    |> then(&calc_distances(galaxies, empty_rows, empty_cols, sum + &1))
+  end
+
+  def empty_mod(a, b, empties) do
+    Enum.reduce(empties, 0, fn empty, acc ->
+      cond do
+        a < empty and empty < b -> acc + 1
+        a > empty and empty > b -> acc + 1
+        true -> acc
+      end
     end)
   end
 end
