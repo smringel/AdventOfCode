60852a0d29811fc0af597b6397a802f2adfb76f8
smringel <seth.ringel@covermymeds.com>
2023-12-14 15:43:16 -0500

    refactor

--- lib/d14c1.ex
+++ lib/d14c1.ex
@@ -3,33 +3,40 @@ defmodule D14C1 do
 
   def run(ext) do
     Parser.parse("d14/#{ext}", false)
-    |> roll_north()
+    |> Enum.map(&String.graphemes/1)
+    |> tilt_north()
     |> weigh()
   end
 
-  def roll_north(map) do
+  def tilt_north(map) do
     map
     |> transpose()
-    |> Enum.map(&Enum.chunk_by(&1, fn val -> val end))
-    |> Enum.reduce([], fn row, acc ->
-      acc ++ [roll_row(row, [])]
-    end)
+    |> tilt()
     |> transpose()
   end
 
-  def roll_row([a, b | tl], acc) do
+  def tilt(map) do
+    map
+    |> Enum.map(&Enum.chunk_by(&1, fn val -> val end))
+    |> Enum.reduce([], fn row, acc ->
+      acc ++ [tilt_row(row, [])]
+    end)
+    |> Enum.map(&String.graphemes/1)
+  end
+
+  def tilt_row([a, b | tl], acc) do
     cond do
-      hd(a) == hd(b) -> roll_row([a ++ b | tl], acc)
-      hd(a) == "." and hd(b) == "O" -> roll_row([a | tl], acc ++ [b])
-      true -> roll_row([b | tl], acc ++ [a])
+      hd(a) == hd(b) -> tilt_row([a ++ b | tl], acc)
+      hd(a) == "." and hd(b) == "O" -> tilt_row([a | tl], acc ++ [b])
+      true -> tilt_row([b | tl], acc ++ [a])
     end
   end
 
-  def roll_row([a], acc), do: acc ++ [a] |> List.flatten() |> Enum.join()
+  def tilt_row([a], acc), do: acc ++ [a] |> List.flatten() |> Enum.join()
 
   def weigh(map) do
     max = length(map)
-    
+
     map
     |> Enum.with_index(&{max - &2, &1})
     |> Enum.reduce(0, fn {weight, row}, acc ->
@@ -39,7 +46,6 @@ defmodule D14C1 do
 
   def transpose(map) do
     map
-    |> Enum.map(&String.graphemes/1)
     |> List.zip()
     |> Enum.map(&Tuple.to_list/1)
     |> Enum.map(&Enum.join/1)
--- lib/d14c2.ex
+++ lib/d14c2.ex
@@ -6,15 +6,19 @@ defmodule D14C2 do
   def run(ext) do
     Parser.parse("d14/#{ext}", false)
     |> Enum.map(&String.graphemes/1)
-    |> then(&Enum.reduce_while(1..@cycles, {nil, nil, [&1]}, fn x, {_, _, prev_maps} ->
+    |> find_cycle()
+    |> select_map()
+    |> weigh()
+  end
+
+  def find_cycle(map) do
+    Enum.reduce_while(1..@cycles, {nil, nil, [map]}, fn x, {_, _, prev_maps} ->
       next = spin_cycle(List.last(prev_maps))
       case Enum.find_index(prev_maps, fn map -> map == next end) do
         nil -> {:cont, {nil, nil, prev_maps ++ [next]}}
         index -> {:halt, {x, index, prev_maps}}
       end
-    end))
-    |> select_map()
-    |> weigh()
+    end)
   end
 
   def select_map({x, cycle_start_x, maps}) do
@@ -26,65 +30,57 @@ defmodule D14C2 do
 
   def spin_cycle(map) do
     map
-    |> roll_north()
-    |> roll_west()
-    |> roll_south()
-    |> roll_east()
+    |> tilt_north()
+    |> tilt_west()
+    |> tilt_south()
+    |> tilt_east()
   end
 
-  def roll_north(map) do
+  def tilt_north(map) do
     map
     |> transpose()
-    |> Enum.map(&Enum.chunk_by(&1, fn val -> val end))
-    |> Enum.reduce([], fn row, acc ->
-      acc ++ [roll_row(row, [])]
-    end)
-    |> Enum.map(&String.graphemes/1)
-    |> transpose
+    |> tilt()
+    |> transpose()
   end
 
-  def roll_west(map) do
-    map
-    |> Enum.map(&Enum.chunk_by(&1, fn val -> val end))
-    |> Enum.reduce([], fn row, acc ->
-      acc ++ [roll_row(row, [])]
-    end)
-    |> Enum.map(&String.graphemes/1)
+  def tilt_west(map) do
+    tilt(map)
   end
 
-  def roll_south(map) do
+  def tilt_south(map) do
     map
     |> transpose()
     |> Enum.map(&Enum.reverse/1)
-    |> Enum.map(&Enum.chunk_by(&1, fn val -> val end))
-    |> Enum.reduce([], fn row, acc ->
-      acc ++ [roll_row(row, [])]
-    end)
-    |> Enum.map(&String.reverse/1)
-    |> Enum.map(&String.graphemes/1)
+    |> tilt()
+    |> Enum.map(&Enum.reverse/1)
     |> transpose()
   end
 
-  def roll_east(map) do
+  def tilt_east(map) do
     map
     |> Enum.map(&Enum.reverse/1)
+    |> tilt()
+    |> Enum.map(&Enum.reverse/1)
+  end
+
+  def tilt(map) do
+    map
     |> Enum.map(&Enum.chunk_by(&1, fn val -> val end))
     |> Enum.reduce([], fn row, acc ->
-      acc ++ [roll_row(row, [])]
+      acc ++ [tilt_row(row, [])]
     end)
-    |> Enum.map(&String.reverse/1)
     |> Enum.map(&String.graphemes/1)
   end
 
-  def roll_row([a, b | tl], acc) do
+  def tilt_row([a, b | tl], acc) do
     cond do
-      hd(a) == hd(b) -> roll_row([a ++ b | tl], acc)
-      hd(a) == "." and hd(b) == "O" -> roll_row([a | tl], acc ++ [b])
-      true -> roll_row([b | tl], acc ++ [a])
+      hd(a) == hd(b) -> tilt_row([a ++ b | tl], acc)
+      hd(a) == "." and hd(b) == "O" -> tilt_row([a | tl], acc ++ [b])
+      true -> tilt_row([b | tl], acc ++ [a])
     end
   end
 
-  def roll_row([a], acc), do: acc ++ [a] |> List.flatten() |> Enum.join()
+  def tilt_row([a], acc), do: acc ++ [a] |> List.flatten() |> Enum.join()
 
   def weigh(map) do
     max = length(map)
