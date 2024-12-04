49cdcc31bf14ecb0497df330c72299efbbcad7ce
smringel <seth.ringel@covermymeds.com>
2024-12-04 17:06:34 -0500

    day1 c1

--- lib/2024/day1.ex
+++ lib/2024/day1.ex
@@ -1,21 +1,29 @@
 defmodule Day1 do
   import Utils.Parser
 
-  @num_dict %{
-    "one" => "1",
-    "two" => "2",
-    "three" => "3",
-    "four" => "4",
-    "five" => "5",
-    "six" => "6",
-    "seven" => "7",
-    "eight" => "8",
-    "nine" => "9",
-    "zero" => "0"
-  }
-
   def run(file) do
-    parse("d1/day1/#{file}")
-    |> IO.puts()
+    read("day1/#{file}", "2024")
+    |> compile_lists()
+    |> Enum.map(& Enum.sort(&1))
+    |> add_distances()
+    |> then(&IO.puts(inspect(&1)))
+  end
+
+  defp compile_lists(string_array) do
+    string_array
+    |> Enum.reduce([[], []], fn list_row, [list1_acc, list2_acc] ->
+      values = list_row
+      |> String.split("   ")
+      |> Enum.map(& get_int(&1))
+
+      [list1_acc ++ [List.first(values)], list2_acc ++ [List.last(values)]]
+    end)
+  end
+
+  defp add_distances(lists) do
+    Enum.zip_reduce(lists, 0, fn [el1, el2], acc ->
+      dif = abs(el1 - el2)
+      dif + acc
+    end)
   end
 end
--- lib/utils/parser.ex
+++ lib/utils/parser.ex
@@ -1,8 +1,14 @@
 defmodule Utils.Parser do
   @integers ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
-  def parse(ext, trim \\ true) do
-    "lib/data/#{ext}.txt"
+  def parse(ext, trim \\ true, year \\ "2023") do
+    "lib/#{year}/data/#{ext}.txt"
+    |> File.read!()
+    |> String.split("\n", trim: trim)
+  end
+
+  def read(ext, year, trim \\ true) do
+    "lib/#{year}/data/#{ext}.txt"
     |> File.read!()
     |> String.split("\n", trim: trim)
   end
