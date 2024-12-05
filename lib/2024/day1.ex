b5270544a0186bb683425229cec229ceb190bbf2
smringel <seth.ringel@covermymeds.com>
2024-12-05 09:31:33 -0500

    day1 part 2

--- lib/2024/day1.ex
+++ lib/2024/day1.ex
@@ -1,11 +1,19 @@
 defmodule Day1 do
   import Utils.Parser
 
-  def run(file) do
+  def run_part1(file) do
     read("day1/#{file}", "2024")
     |> compile_lists()
     |> Enum.map(& Enum.sort(&1))
     |> add_distances()
+    |> then(&IO.puts(inspect(&1)))
+  end
+
+  def run_part2(file) do
+    read("day1/#{file}", "2024")
+    |> compile_lists()
+    |> Enum.map(& Enum.sort(&1))
+    |> add_appearances()
     |> then(&IO.puts(inspect(&1)))
   end
 
@@ -24,6 +32,12 @@ defmodule Day1 do
     Enum.zip_reduce(lists, 0, fn [el1, el2], acc ->
       dif = abs(el1 - el2)
       dif + acc
+    end)
+  end
+
+  defp add_appearances([list1, list2]) do
+    Enum.reduce(list1, 0, fn list1_number, acc ->
+      Enum.count(list2, & &1 == list1_number) * list1_number + acc
     end)
   end
 end
