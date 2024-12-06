483d26e7798cd8d42913a1fe7fab9eeb6aa848f1
smringel <seth.ringel@covermymeds.com>
2024-12-05 23:00:58 -0500

    day3 part 2

--- lib/2024/day3.ex
+++ lib/2024/day3.ex
@@ -3,21 +3,23 @@ defmodule Day3 do
 
   def run_part1(file) do
     read("day3/#{file}", "2024")
-    |> Enum.reduce(0, &match_all_pattern(&1) + &2)
+    |> Enum.reduce(0, &match_pattern(&1) + &2)
     |> then(&IO.puts(inspect(&1)))
   end
 
   def run_part2(file) do
     read("day3/#{file}", "2024")
-    |> Enum.map(&remove_donts/1)
-    |> Enum.reduce(0, &match_all_pattern(&1) + &2)
+    |> Enum.reduce({[], true}, fn line, {acc, do?} ->
+      {do_vals, end_do?} = remove_donts(line, do?)
+      {[do_vals] ++ acc, end_do?}
+    end)
+    |> elem(0)
+    |> Enum.reduce(0, &match_pattern(&1) + &2)
     |> then(&IO.puts(inspect(&1)))
   end
 
-  @pattern ~r/mul\((-?\d+),(-?\d+)\)/
-
-  defp match_all_pattern(string) do
-    @pattern
+  defp match_pattern(string) do
+    ~r/mul\((-?\d+),(-?\d+)\)/
     |> Regex.scan(string)
     |> Enum.reduce(0, fn match, acc ->
       [a, b] = match
@@ -28,11 +30,16 @@ defmodule Day3 do
     end)
   end
 
-  defp remove_donts(string) do
-    ~r/do/
-    |> Regex.split(string)
-    |> Enum.reject(&Regex.match?(~r/^n't()/, &1))
-    |> Enum.filter(&Regex.match?(~r/^()/, &1))
+  defp remove_donts(string, do?) do
+    split_strings = Regex.split(~r/do/, string)
+    |> then(&if do?, do: &1, else: List.delete_at(&1, 0))
+
+    do_string = split_strings
+    |> Enum.reject(&Regex.match?(~r/^n't\(\)/, &1))
     |> Enum.join("")
+
+    end_op = !Regex.match?(~r/^n't\(\)/, List.last(split_strings))
+
+    {do_string, end_op}
   end
 end
