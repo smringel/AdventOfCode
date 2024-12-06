da0c85e81f0ae1c45b4e5cf09955bde5012c9576
smringel <seth.ringel@covermymeds.com>
2024-12-05 22:15:50 -0500

    passing test

--- lib/2024/day3.ex
+++ lib/2024/day3.ex
@@ -3,17 +3,21 @@ defmodule Day3 do
 
   def run_part1(file) do
     read("day3/#{file}", "2024")
-    |> Enum.reduce(0, &match_pattern(&1) + &2)
+    |> Enum.reduce(0, &match_all_pattern(&1) + &2)
     |> then(&IO.puts(inspect(&1)))
   end
 
   def run_part2(file) do
     read("day3/#{file}", "2024")
+    |> Enum.map(&remove_donts/1)
+    |> Enum.reduce(0, &match_all_pattern(&1) + &2)
     |> then(&IO.puts(inspect(&1)))
   end
 
-  defp match_pattern(string) do
-    ~r/mul\((-?\d+),(-?\d+)\)/
+  @pattern ~r/mul\((-?\d+),(-?\d+)\)/
+
+  defp match_all_pattern(string) do
+    @pattern
     |> Regex.scan(string)
     |> Enum.reduce(0, fn match, acc ->
       [a, b] = match
@@ -22,5 +26,13 @@ defmodule Day3 do
 
       a * b + acc
     end)
+  end
+
+  defp remove_donts(string) do
+    ~r/do/
+    |> Regex.split(string)
+    |> Enum.reject(&Regex.match?(~r/^n't()/, &1))
+    |> Enum.filter(&Regex.match?(~r/^()/, &1))
+    |> Enum.join("")
   end
 end
